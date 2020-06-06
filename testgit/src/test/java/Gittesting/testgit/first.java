01f70692a0a7687d6e6906c79eab192cad438a7f
safewayqa <36218800+safewayqa@users.noreply.github.com>
2020-06-06 20:19:07 +0530

    New code

---
+++ testgit/src/test/java/Gittesting/testgit/first.java
@@ -0,0 +1,19 @@
+package Gittesting.testgit;
+
+import org.testng.annotations.Test;
+
+public class first {
+
+	
+	@Test
+	public void team() {
+		
+		
+		System.out.println("Nargees");
+		System.out.println("Rehan");
+		System.out.println("Ridhu");
+	}
+	
+	
+	
+}
