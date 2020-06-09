12e10e10f53523272736c342b47163f601faa65a
safewayqa <36218800+safewayqa@users.noreply.github.com>
2020-06-09 23:07:12 +0530

    multi url added

--- testgit/src/test/java/Gittesting/testgit/first.java
+++ testgit/src/test/java/Gittesting/testgit/first.java
@@ -2,17 +2,41 @@ package Gittesting.testgit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class first {
 
+	public 	WebDriver driver;
 	
+	@Parameters("browsers")
 	@Test
-	public void team() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rehan\\git\\RehanRidhu\\testgit\\browsers\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+	public void team(String browsers) {
 		
-		driver.get("https://www.shopjustice.com/");
+		
+		
+	if(browsers.equalsIgnoreCase("chrome")) {
+		
+		
+		
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\rehan\\git\\RehanRidhu\\testgit\\browsers\\chromedriver.exe");
+		   driver=new ChromeDriver();
+	} else if(browsers.equalsIgnoreCase("firefox")) {
+		  
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\rehan\\git\\RehanRidhu\\testgit\\browsers\\geckodriver.exe");
+			driver=new FirefoxDriver();
+	}
+		/*
+		 * System.setProperty("webdriver.ie.driver",
+		 * "C:\\Users\\rehan\\git\\RehanRidhu\\testgit\\browsers\\IEDriverServer.exe");
+		 * WebDriver driver=new InternetExplorerDriver();
+		 * 
+		 * 
+		 */
+		
+			driver.get("https://www.shopjustice.com/");
 		System.out.println("Nargees");
 		System.out.println("Rehan");
 		System.out.println("Ridhu sanuu");
